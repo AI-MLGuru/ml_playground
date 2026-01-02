0e210289b694295e20bc8699a45f256da6968e4b
Olamide Fadahunsi <midefadahunsi@gmail.com>
2026-01-02 14:15:00 +0100

    Step 2: Extract app configuration into utils module i.e creating a seperate module that it's function is to introduce the systems configuration

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import tkinter as tk
+from src.utils import get_app_config
 
 def main():
+  config = get_app_config()
   root = tk.Tk()
   root.title("ML Playground")
-  root.geometry("400x300")
+  root.geometry(f"{config['width']}x{config['height']}")
   
   label = tk.Label(
     root,
-    text = "ML Playground\nFoundation step",
+    text = config["welcome_text"],
     font = ("Arial", 14)
   )
   label.pack(expand=True)
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+def get_app_config():
+  return{
+    "title": "ML Playground",
+    "width": 400,
+    "height": 300,
+    "welcome_text": "ML Playground\nFoundation Step"
+  }
